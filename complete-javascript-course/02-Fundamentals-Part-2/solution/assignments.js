4c4b21bdbc9ef17f0f564295d57c8be400026d3c
Do Thien Hoang <theksbd@gmail.com>
2022-10-19 00:38:54 +0700

    Complete assignments until lecture 35 Arrow Functions

---
+++ complete-javascript-course/02-Fundamentals-Part-2/solution/assignments.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// function describeCountry(country, population, capitalCity) {
+//   return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+// }
+
+// console.log(describeCountry("Viet Nam", 925, "Hanoi"));
+// console.log(describeCountry("Finland", 6, "Helsinki"));
+// console.log(describeCountry("Japan", 100, "Tokyo"));
+
+// function percentageOfWorld1(population) {
+//   const populationOfWorld = 7900;
+//   return `${(population / populationOfWorld) * 100}%`;
+// }
+
+// console.log(percentageOfWorld1(1440));
+
+// const percentageOfWorld2 = function (population) {
+//   const populationOfWorld = 7900;
+//   return `${(population / populationOfWorld) * 100}%`;
+// };
+
+// console.log(percentageOfWorld2(1440));
+
+// const percentageOfWorld3 = (population) => {
+//     const populationOfWorld = 7900;
+//     return `${(population / populationOfWorld) * 100}%`;
+// };
+// console.log(percentageOfWorld3(1440));
