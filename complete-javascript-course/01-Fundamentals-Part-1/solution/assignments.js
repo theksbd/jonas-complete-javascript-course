17389c346f8c5dc5d1d2f04beefe0491019be3db
Do Thien Hoang <theksbd@gmail.com>
2022-10-18 16:25:26 +0700

    Complete assignments until lecture 21. Truthy and Falsy values

---
+++ complete-javascript-course/01-Fundamentals-Part-1/solution/assignments.js
@@ -0,0 +1,26 @@
+const country = "Vietnam";
+const continent = "Asia";
+const population = 925;
+const isIsland = false;
+let language;
+language = "Vietnamese";
+
+const populationInSouthVN = population / 2;
+const populationInNorthVN = population / 2;
+
+const description = `${country} in in ${continent} and its ${population} million people speak ${language}`;
+// console.log(description);
+
+// if (population > 33) {
+//   console.log(`${country}'s population is above average`);
+// } else {
+//   console.log(
+//     `${country}'s population is ${33 - population} million below average`
+//   );
+// }
+
+// console.log("9" - "5"); // 4
+// console.log("19" - "13" + "17"); // 617
+// console.log("19" - "13" + 17); // 23
+// console.log("123" < 57); // false
+// console.log(5 + 6 + "4" + 9 - 4 - 2); // 1143
---
+++ complete-javascript-course/01-Fundamentals-Part-1/solution/script.js
@@ -0,0 +1,17 @@
+// // Coding Challenge 1
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+// const markBMI = markMass / markHeight ** 2;
+// const johnBMI = johnMass / johnHeight ** 2;
+
+// const markHigherBMI = markBMI > johnBMI;
+
+// // Coding Challenge 2
+// if (markHigherBMI) {
+//   console.log(`Mark's BMI (${markBMI}) is higher than John's! (${johnBMI})`);
+// } else {
+//   console.log(`John's BMI (${johnBMI}) is higher than Mark's! (${markBMI})`);
+// }
