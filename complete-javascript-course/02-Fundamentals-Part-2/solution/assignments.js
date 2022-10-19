dc1e4faf003fb4ec2666ef55d7233423953ae3d8
Do Thien Hoang <theksbd@gmail.com>
2022-10-20 01:11:21 +0700

    Complete assignments to lecture 42 Introduction to Objects

--- complete-javascript-course/02-Fundamentals-Part-2/solution/assignments.js
+++ complete-javascript-course/02-Fundamentals-Part-2/solution/assignments.js
@@ -28,3 +28,45 @@
 // };
 
 // console.log(percentageOfWorld3(1440));
+
+// const describePopulation = (country, population) => {
+//   return `${country} has ${population} million people, which is about ${percentageOfWorld3(
+//     population
+//   )} of the world`;
+// };
+
+// console.log(describePopulation("Viet Nam", 925));
+
+// const populations = [925, 110, 6, 1440];
+// console.log(populations.length === 4);
+// const percentages = [
+//   percentageOfWorld1(populations[0]),
+//   percentageOfWorld1(populations[1]),
+//   percentageOfWorld1(populations[2]),
+//   percentageOfWorld1(populations[3]),
+// ];
+// console.log(percentages);
+
+// const neighbors = ["Thailand", "Laos", "Cambodia"];
+// console.log(neighbors);
+// neighbors.push("Utopia");
+// console.log(neighbors);
+// neighbors.pop();
+// console.log(neighbors);
+// if (neighbors.includes("Germany")) {
+//   console.log("Probably not a central European country :D");
+// }
+// // Method 1: use findIndex
+// neighbors[neighbors.findIndex((neighbor) => neighbor === "Laos")] = "Sweden";
+// console.log(neighbors);
+// // Method 2: use indexOf
+// neighbors[neighbors.indexOf("Laos")] = "Sweden";
+// console.log(neighbors);
+
+// const myCountry = {
+//   country: "Viet Nam",
+//   capital: "Ha noi",
+//   language: "Vietnamese",
+//   population: 925,
+//   neighbors: ["Thailand", "Laos", "Cambodia"],
+// };
--- complete-javascript-course/02-Fundamentals-Part-2/solution/script.js
+++ complete-javascript-course/02-Fundamentals-Part-2/solution/script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+// // Coding Challenge 1
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+// const dolphinsScore = calcAverage(85, 54, 41);
+// const koalasScore = calcAverage(23, 34, 27);
+
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//   if (avgDolphins >= avgKoalas * 2) {
+//     return `Dolphins win (${dolphinsScore} vs. ${koalasScore})`;
+//   }
+//   if (avgKoalas >= avgDolphins * 2)
+//     return `Koalas win (${koalasScore} vs. ${dolphinsScore})`;
+//   return "No one wins";
+// };
+
+// console.log(checkWinner(dolphinsScore, koalasScore));
+
+// // Coding Challenge 2
+// const calcTip = (bill) => {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+// console.log(bills);
+// console.log(tips);
+// console.log(total);
