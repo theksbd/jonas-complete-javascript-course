3f9b16652152e1f3f2eebd7b741693ec77b01b3f
Do Thien Hoang <theksbd@gmail.com>
2022-11-18 21:16:44 +0700

    Complete course until lecture 149 Map, filter and reduce method

---
+++ complete-javascript-course/11-Arrays-Bankist/solution/challenge.js
@@ -0,0 +1,24 @@
+/* ------ Coding Challenge 1 ------ */
+// let dogsJulia = [3, 5, 2, 12, 7];
+// let dogsKate = [4, 1, 15, 8, 3];
+
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   const dogsJuliaCopy = dogsJulia.slice();
+//   dogsJuliaCopy.splice(0, 1);
+//   dogsJuliaCopy.splice(-2);
+//   console.log(dogsJulia);
+//   console.log(dogsJuliaCopy);
+//   const dogs = dogsJuliaCopy.concat(dogsKate);
+//   console.log(dogs);
+//   dogs.forEach((dog, i) => {
+//     dog >= 3
+//       ? console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`)
+//       : console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+//   });
+// };
+
+// checkDogs(dogsJulia, dogsKate);
+
+// dogsJulia = [9, 16, 6, 8, 3];
+// dogsKate = [10, 5, 6, 1, 4];
+// checkDogs(dogsJulia, dogsKate);
--- complete-javascript-course/11-Arrays-Bankist/solution/script.js
+++ complete-javascript-course/11-Arrays-Bankist/solution/script.js
@@ -159,3 +159,10 @@ displayMovement(account1.movements);
 // currenciesSet.forEach((value, key, set) => {
 //   console.log(`Key ${key}: ${value}`);
 // });
+
+// map, filter and reduce
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+const movementsUSD = movements.map((mov) => mov * eurToUsd);
+console.log(movements);
+console.log(movementsUSD);
