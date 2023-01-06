e41aa2061fb303a8830d5233d64713ccd5b556e4
Do Thien Hoang <theksbd@gmail.com>
2023-01-07 01:03:15 +0700

    Complete course until lecture 192 Event Delegation: Implementing Page Navigation

--- complete-javascript-course/13-Advanced-DOM-Bankist/solution/script.js
+++ complete-javascript-course/13-Advanced-DOM-Bankist/solution/script.js
@@ -52,7 +52,55 @@ btnScrollTo.addEventListener("click", function (e) {
   section1.scrollIntoView({ behavior: "smooth" });
 });
 
-const h1 = document.querySelector("h1");
-h1.addEventListener("mouseenter", function (e) {
-  alert("Hello");
+// Page navigation
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
 });
+
+/* -------------- Experiments ----------------- */
+
+// const h1 = document.querySelector("h1");
+// const handleEventH1 = function (e) {
+//   console.log("H1");
+//   h1.removeEventListener("mouseenter", handleEventH1);
+// };
+// h1.addEventListener("mouseenter", handleEventH1);
+
+// const randomInt = (min, max) =>
+//   Math.floor(min + Math.random() * (max - min + 1));
+
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+// document.querySelector(".nav__link").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log("LINK", e.target);
+//   console.log(e.currentTarget === this);
+//   e.stopPropagation();
+// });
+
+// document.querySelector(".nav__links").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log(" CONTAINER", e.target);
+// });
+
+// document.querySelector(".nav").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log("NAV", e.target);
+// });
