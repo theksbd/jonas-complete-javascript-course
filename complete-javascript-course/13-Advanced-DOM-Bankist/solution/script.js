5c74ab563bf4e40b0f76ba620bde3b29457bb84d
Do Thien Hoang <theksbd@gmail.com>
2023-01-12 18:48:33 +0700

    Complete course until lecture 197 A better Way: The Intersection Observer API

--- complete-javascript-course/13-Advanced-DOM-Bankist/solution/script.js
+++ complete-javascript-course/13-Advanced-DOM-Bankist/solution/script.js
@@ -7,6 +7,12 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+const nav = document.querySelector(".nav");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -31,9 +37,6 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
-
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
 
 btnScrollTo.addEventListener("click", function (e) {
   // const s1coords = section1.getBoundingClientRect();
@@ -72,6 +75,84 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+// Tabbed component
+tabsContainer.addEventListener("click", function (e) {
+  // Method 1: ternary operator
+  // const clickedTab =
+  //   e.target.parentElement === tabsContainer
+  //     ? e.target
+  //     : e.target.parentElement;
+
+  // Method 2: closest()
+  const clickedTab = e.target.closest(".operations__tab");
+
+  if (!clickedTab) return;
+  // Remove active class
+  tabs.forEach(tab => tab.classList.remove("operations__tab--active"));
+  tabsContent.forEach(tab =>
+    tab.classList.remove("operations__content--active")
+  );
+  // Activate tab
+  clickedTab.classList.add("operations__tab--active");
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clickedTab.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const clickedLink = e.target;
+    const siblings = clickedLink.closest(".nav").querySelectorAll(".nav__link");
+    const logo = clickedLink.closest(".nav").querySelector("img");
+
+    siblings.forEach(el => {
+      if (el !== clickedLink) {
+        el.style.opacity = this;
+      }
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+
+// Sticky Navigation
+// Method 1: Naive approach
+// const initialCoords = section1.getBoundingClientRect();
+// window.addEventListener("scroll", function (e) {
+//   if (this.window.scrollY > initialCoords.top) {
+//     nav.classList.add("sticky");
+//   } else {
+//     nav.classList.remove("sticky");
+//   }
+// });
+
+// Method 2: Intersection Observer API
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`
+});
+headerObserver.observe(header);
+
+// Reveal sections
+
 
 ///////////////////////////////////////
 // Experiments
@@ -131,3 +212,18 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 //     el.style.transform = "scale(0.5)";
 //   }
 // });
+
+// The Intersection Observer API
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2]
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
